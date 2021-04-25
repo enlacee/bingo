a8296907bc3930a13a195381528c9a475019d5bf
Anibal Copitan <anibal@pprios.com>
2021-04-25 18:37:27 -0500

    [add] valid Calling the method

--- tests/CallingBingoNumbersTest.php
+++ tests/CallingBingoNumbersTest.php
@@ -18,33 +18,8 @@ final class CallingBingoNumbersTest extends TestCase {
     public function testMethodIsAvailable() {
         
         $obj = new CallingBingoNumbers();
-        $this->assertIsNotCallable($obj);
+        $this->assertIsNotCallable($obj);;
     }
 
-    /**
-     * Validation if the result is type integer
-     * 
-     * @return void
-     */
-    public function testTheResultIsNumber() {
-        
-        $obj = new CallingBingoNumbers();
-        $valueNumber = $obj->callOneNumber();
 
-        $this->assertIsInt($valueNumber);
-    }
-
-    /**
-     * Validation of range allowed [1-75]
-     * 
-     * @return void
-     */
-    public function testRangeNumberIsValid() {
-
-        $obj = new CallingBingoNumbers();
-        $valueNumber = $obj->callOneNumber();
-        $range = range(1, 75);
-
-        $this->assertContains($valueNumber, $range);
-    }
 }
